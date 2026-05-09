bfa4bdff8301dda628947fc26fcab7dbd33ea589
dinogamboa111 <din.gamboa@duocuc.cl>
2026-05-09 10:34:00 -0400

    se añade back end
    se añade carpeta geenrada con springboot inizializer,
    -JAVA 17
    -MAVEN
    -JAR

---
+++ Back-end/kydira-api/kydira-api/src/main/java/com/kydira_api/KydiraApiApplication.java
@@ -0,0 +1,13 @@
+package com.kydira_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KydiraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KydiraApiApplication.class, args);
+	}
+
+}
---
+++ Back-end/kydira-api/kydira-api/src/test/java/com/kydira_api/KydiraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kydira_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KydiraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
